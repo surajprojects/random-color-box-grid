8d2a4ba3b8c6d00b0a68404c283f846382319b19
TIGER <suraj23082002@gmail.com>
2025-01-31 15:22:08 +0530

    added gh pages for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/random-color-box-grid/',  // This should match your GitHub repo name
   plugins: [react()],
 })
